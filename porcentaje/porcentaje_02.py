8a84a295b4017cce00f51c58bad31b2ca25f8615
alegaon <alegaon@outlook.com.ar>
2022-05-06 01:12:16 -0300

    finalizado

--- porcentaje/porcentaje_02.py
+++ porcentaje/porcentaje_02.py
@@ -4,20 +4,21 @@ def total(resultado_del_porcentaje, porcentaje):
     return (resultado_del_porcentaje * 100) / porcentaje  
 
 def resultado_del_porcentaje(total, porcentaje):
-    return (total * 100 ) / porcentaje
+    return total * (porcentaje / 100)
 
 def porcentaje(resultado_del_porcentaje, total):
-    return  resultado_del_porcentaje / (total * 100)
+    return (resultado_del_porcentaje * 100) / total
 
 # otras funciones para hacer los input de numeros
 
 def ingresar_numero(dato):
     while True:
         try:
-            numero = int(input(f"Escribi tu {dato} buscado: "))
+            numero = float(input(f"Escribi tu {dato}: "))
             break
         except ValueError:
-            print("DALE PAH!.. q paso? Solo escribi numeros")
+            print("ERROR")
+            print("DALE PAPEH!.. q paso? Solo escribi numeros")
             print("ej: 1 , 2 , 454654... okas??")
             print("vamo' de nuevo")
     return numero
@@ -27,7 +28,7 @@ def ingresar_numero(dato):
 while True:
     print("/////////////////////////////")
     print("")
-    print("eleji la variable a despejar:")
+    print("elegi la variable a despejar:")
     print("""
     1 - total
     2 - resultado del porcentaje
@@ -41,6 +42,8 @@ while True:
 
     # 4 para salir
     if opcion == "4":
+        print("gracias keridisimx")
+        print("bye bye!")
         break
 
     # 1 despeja tota
@@ -50,4 +53,20 @@ while True:
         tu_porcentaje = ingresar_numero("porcentaje")
         tu_total = total(tu_resultado_porcentaje, tu_porcentaje)
         print(f"tu total es: {tu_total}")
-        
+
+    # 2 despeja resultado del porcentaje
+    if opcion == "2":
+        print("resultado del porcentaje")
+        tu_total = ingresar_numero("total")
+        tu_porcentaje = ingresar_numero("porcentaje")
+        tu_resultado_del_porcentaje = resultado_del_porcentaje(tu_total, tu_porcentaje)
+        print(f"tu resultado del porcentaje es: {tu_resultado_del_porcentaje}")
+
+
+    # 3 despejar %
+    if opcion == "3":
+        print("porcentaje")  
+        tu_resultado_del_porcentaje = ingresar_numero("resultado del porcentaje")
+        tu_total = ingresar_numero("total")
+        tu_porcentaje = porcentaje(tu_resultado_del_porcentaje, tu_total)
+        print(f"tu porcentaje es: {tu_porcentaje}")
