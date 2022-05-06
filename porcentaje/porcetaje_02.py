4c57495505e62a328da32266232e3f60dc67e7b9
alegaon <alegaon@outlook.com.ar>
2022-05-05 23:49:24 -0300

    porcentaje_02

--- porcentaje/porcetaje_02.py
+++ porcentaje/porcetaje_02.py
@@ -1,1 +1,10 @@
-porcetaje_02.py
+# buscando variables
+
+def total(numero, porcentaje):
+    return  (numero * porcentaje) / 100
+
+def numero(total, porcentaje):
+    return (total * 100 ) / porcentaje
+
+def porcentaje(numero, total):
+    return  numero / (total * 100)
